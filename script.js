a0381a5f81f97fa073846cc5fac1125e95bf1fc1
mohdhanif1234 <88095538+mohdhanif1234@users.noreply.github.com>
2023-05-25 20:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("btnSelectRemove").addEventListener("click", function(){
+	const colorSelect = document.getElementById("colorSelect");
+	colorSelect.firstElementChild.remove();
+})
